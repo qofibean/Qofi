72f7fbad571230bc68a295462c3a3616a13974f8
Yonkokilasi <yonkokilasi@gmail.com>
2017-03-24 15:54:38 +0300

    Addition of function that will calculate the total

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -1,8 +1,13 @@
 $(document).ready()
 
+var total = function (producttype, quantity) {
+    return producttype * quantity
+};
+
 function {
     $("#10ml").click(function {
+        var producttype = 3000;
+    });
 
-    })
 
 }
