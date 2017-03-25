9be9b93f9cf771094053c4c315b4eac01f42d717
yonkokilasi <yonkokilasi@gmail.com>
2017-03-25 16:02:33 +0300

    update of order.html with a form

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -1,12 +1,27 @@
-var total = function (producttype, quantity) {
-    return producttype * quantity
+var multiply=function(number1,number2){
+return number1 * number2
 };
-$(document).ready()
-
-function ({
-    $("#10ml").click(function {
-        var producttype = 3000;
+$(document).ready(function() {
+    $("button#10ml").click(function() {
+        var number1= 3000
+        console.log(number1)
+        })
+    $("#quantity").submit(function(event){
+        event.preventDefault();
+        var number2= parseInt($("#more").val())
+   console.log(number2);
     });
+   
+    $("#equal").click(function() {
+var payment = multiply(number2,number1) 
+ console.log(payment);
+        $("h1#total").text("Your total is " + payment)
+    });
+    
+    }); 
+
+                
 
 
-});
+
+// var number1 = parseInt($("#add1").val());
