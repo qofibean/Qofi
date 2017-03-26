521714cb5f596d9d3323d5ba30b50a1a0feb8c41
yonkokilasi <yonkokilasi@gmail.com>
2017-03-26 14:27:09 +0300

    Addition of product .html where the description of a product will be placed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-    $("#read").click(function () {
-        $("#oilhide").show();
-        $("#read").hide();
-        console.log("chapo");
-    });
-    $("#less").click(function () {
-        $("#oilhide").hide();
-        $("#read").hide();
-        console.log("chapo");
-    })
-});
+//$(document).ready(function () {
+//    $("#read").click(function () {
+//        $("#oilhide").show();
+//        $("#read").hide();
+//        console.log("chapo");
+//    });
+//    $("#less").click(function () {
+//        $("#oilhide").hide();
+//        $("#read").hide();
+//        console.log("chapo");
+//    })
+//});
